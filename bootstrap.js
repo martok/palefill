d3ce4a6506b62c67c3db2e22c09213ca382b74d7
Martok <martok@martoks-place.de>
2022-06-08 20:46:11 +0200

    add: detect at runtime what kind of app we're running on

--- bootstrap.js
+++ bootstrap.js
@@ -11,6 +11,13 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
 
+const PLATFORM_SUPPORTED_PRODUCTS = [                          // Apps specifically targeted by install.rdf etc.
+    "{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}",                  // Pale Moon
+    "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}",                  // Basilisk / Iceweasel-UXP
+    "{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}",                  // SeaMonkey
+];
+const PLATFORM_UXP_MAXVER = "10.*";                            // arbitrarily chosen
+
 let gAddonData = null;
 
 function require(module)
@@ -28,24 +35,54 @@ function require(module)
 }
 require.scopes = Object.create(null);
 
+function _validateRuntime(data) {
+    const id = Services.appinfo.ID.toLowerCase();
+    if (PLATFORM_SUPPORTED_PRODUCTS.includes(id)) {
+        // definitely a supported app
+        data.runtimeMode = "SUPPORTED";
+    } else if (Services.vc.compare(Services.appinfo.platformVersion, PLATFORM_UXP_MAXVER) < 0) {
+        // highly likely something building on UXP/Goanna
+        data.runtimeMode = "UXP-GENERIC";
+    } else {
+        // any other Mozilla product. Not our place to be.
+        data.runtimeMode = "INVALID";
+    }
+}
+
 function startup(data, reason) {
+    _validateRuntime(data);
     gAddonData = data;
-    const settings = require("settings").getService();
-    settings.init("extensions.palefill.", gAddonData);
-    settings.setDefaults(require("settings-defaults"));
-    require("main").init();
+    if (gAddonData.runtimeMode !== "INVALID") {
+        const settings = require("settings").getService();
+        settings.init("extensions.palefill.", gAddonData);
+        settings.setDefaults(require("settings-defaults"));
+        require("main").init();
+        if (reason === ADDON_INSTALL && data.runtimeMode === "UXP-GENERIC") {
+            // on first install, go to settings where we show the compat warning
+            require("settings").getService().openAddonSettings();
+        }
+    }
 }
 
 function shutdown(data, reason) {
     if (reason == APP_SHUTDOWN) {
         return;
     }
-    require("main").done();
-    require("settings").getService().done();
-    require.scopes = {};
+    if (gAddonData.runtimeMode !== "INVALID") {
+        require("main").done();
+        require("settings").getService().done();
+        require.scopes = {};
+    }
     gAddonData = null;
 }
 
-function install() {}
+function install(data, reason) {
+    if (reason === ADDON_INSTALL) {
+        _validateRuntime(data);
+        if (data.runtimeMode === "INVALID") {
+            alert("Palefill", "Palefill was just installed on a platform that it does not run on. You may want to disable/remove it, it will not function.");
+        }
+    }
+}
 
 function uninstall() {}
